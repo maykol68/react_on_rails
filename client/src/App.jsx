75f7235fa6744bf6e8a68ba549f1db89fa571ee1
maymine <sm9365277@gmail.com>
2023-09-01 22:41:30 -0500

    18:18 / 20:43
    The Vite App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
     <div className="app">
       <h1>React on Rails Blog</h1>
       <p>Find this application layout in client/src/App.jsx</p>
+      
       <PostsList />
+
     </div>
   
   </>
