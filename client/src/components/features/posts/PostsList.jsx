2d390ef56d05c0d5db52b9235c57ffb04fe3c4fe
maymine <sm9365277@gmail.com>
2023-09-01 22:39:01 -0500

    18:18 / 20:43
    The Vite App

---
+++ client/src/components/features/posts/PostsList.jsx
@@ -0,0 +1,67 @@
+// API_URL comes from the .env.development file
+import React, { useState, useEffect } from "react";
+
+
+function PostsList() {
+  const [posts, setPosts] = useState([]);
+  const [, setLoading] = useState(true);
+  const [, setError] = useState(null);
+  // Fetch posts from the API
+  useEffect(() => {
+    async function loadPosts() {
+      try {
+        const response = await fetch(API_URL);
+        if (response.ok) {
+          const json = await response.json();
+          setPosts(json);
+        } else {
+          throw response;
+        }
+      } catch (e) {
+        setError("An error occurred. Awkward...");
+        console.log("An error occurred:", e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadPosts();
+  }, []);
+
+  const deletePost = async (id) => {
+    try {
+      // DELETE request to: http://localhost:3000/api/v1/posts/:id
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        setPosts(posts.filter((post) => post.id !== id));
+      } else {
+        throw response;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-container">
+          <h2>
+            <Link to={`/posts/${post.id}`} className="post-title">
+              {post.title}
+            </Link>
+          </h2>
+          <div className="post-links">
+            <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+            {" | "}
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PostsList;
