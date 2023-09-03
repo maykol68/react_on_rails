86f70d73cbfd77577afa0741ab14c2aed6d19fd3
maymine <sm9365277@gmail.com>
2023-09-03 10:31:50 -0500

    the vite app -v3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,13 @@ import './App.css'
 import PostsList from './components/features/posts/PostsList';
 
 function App() {
- 
-
   return (
   <>
     <div className="app">
       <h1>React on Rails Blog</h1>
       <p>Find this application layout in client/src/App.jsx</p>
-      
       <PostsList />
-
     </div>
-  
   </>
   );
 }
--- client/src/components/features/posts/PostsList.jsx
+++ client/src/components/features/posts/PostsList.jsx
@@ -1,5 +1,6 @@
 // API_URL comes from the .env.development file
 import React, { useState, useEffect } from "react";
+import { API_URL } from '../../../constants';
 
 
 function PostsList() {
@@ -25,39 +26,13 @@ function PostsList() {
       }
     }
     loadPosts();
-  }, );
-
-  const deletePost = async (id) => {
-    try {
-      // DELETE request to: http://localhost:3000/api/v1/posts/:id
-      const response = await fetch(`${API_URL}/${id}`, {
-        method: "DELETE",
-      });
-
-      if (response.ok) {
-        setPosts(posts.filter((post) => post.id !== id));
-      } else {
-        throw response;
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
+  }, []);
   return (
     <div>
       {posts.map((post) => (
         <div key={post.id} className="post-container">
-          <h2>
-            <Link to={`/posts/${post.id}`} className="post-title">
-              {post.title}
-            </Link>
-          </h2>
-          <div className="post-links">
-            <Link to={`/posts/${post.id}/edit`}>Edit</Link>
-            {" | "}
-            <button onClick={() => deletePost(post.id)}>Delete</button>
-          </div>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
         </div>
       ))}
     </div>
