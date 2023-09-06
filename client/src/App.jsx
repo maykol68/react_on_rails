6ba7cb94518005bb1a8c066bdeb77acffeeca87c
maymine <sm9365277@gmail.com>
2023-09-05 23:31:49 -0500

    added Postdetails and AppRoutes components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRoutes as Router } from "react-router-dom";
-import './App.css';
-import PostsList from './components/features/posts/PostsList';
-import Navbar from ".components/NavBar";
-
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import AppRoutes from "./components/AppRoutes";
 function App() {
   return (
   <Router>
     <div className="app">
       <h1>React on Rails Blog</h1>
       <p>Find this application layout in client/src/App.jsx</p>
-      <Navbar />
-      <PostsList />
+      <NavBar />
+      <AppRoutes />
     </div>
   </Router>
   );
 }
 
-export default App
+export default App;
---
+++ client/src/components/features/posts/PostDetails.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+function PostDetails() {
+
+}
+
+
+
+export default PostDetails;
