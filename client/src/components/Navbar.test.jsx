07cbe94e2d9e1c959281f4db31cc6ebce42eda71
maymine <sm9365277@gmail.com>
2023-09-21 17:26:41 -0500

    Testing the Routes Component

--- client/src/components/Navbar.test.jsx
+++ client/src/components/Navbar.test.jsx
@@ -1,17 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom"; // To mock 'Link' component
-import NavBar from "./NavBar.jsx";
+import { MemoryRouter } from "react-router-dom"; // To mock 'Link' components from 'react-router-dom'
+import NavBar from "./NavBar";
 
-describe("Navbar component", () => {
-    const renderNavBar = () => {
-        render(<NavBar />, { wrapper: MemoryRouter });
-    };
-    test("renders both links", () => {
-        // render the navbar
-        renderNavBar();
-        // expect the links to be there or something
-        expect(screen.getByText("Posts List")).toBeInTheDocument();
-        expect(screen.getByText("Create New Post")).toBeInTheDocument();
-
-    });
+describe("NavBar component", () => {
+  const renderNavBar = () => {
+    render(<NavBar />, { wrapper: MemoryRouter });
+  };
+  test("renders both links", () => {
+    // render the navbar
+    renderNavBar();
+    // expect the links to be there or something
+    expect(screen.getByText("Posts List")).toBeInTheDocument();
+    expect(screen.getByText("Create New Post")).toBeInTheDocument();
+  });
 });
