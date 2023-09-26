aaeb74c5301a70eb1dd7deebe43693c1bd5ffefc
maymine <sm9365277@gmail.com>
2023-09-26 05:46:14 -0500

    Tested service

--- client/src/components/services/postService.js
+++ client/src/components/services/postService.js
@@ -1,5 +1,4 @@
 import { API_URL } from "../../constants";
-
 async function fetchAllPosts() {
   const response = await fetch(`${API_URL}`);
   if (!response.ok) {
@@ -7,7 +6,6 @@ async function fetchAllPosts() {
   }
   return response.json();
 }
-
 async function fetchPost(id) {
   const response = await fetch(`${API_URL}/${id}`);
   if (!response.ok) {
@@ -15,7 +13,6 @@ async function fetchPost(id) {
   }
   return response.json();
 }
-
 async function createPost(postData) {
   const response = await fetch(`${API_URL}`, {
     method: "POST",
@@ -24,14 +21,11 @@ async function createPost(postData) {
     },
     body: JSON.stringify(postData),
   });
-
   if (!response.ok) {
     throw new Error(response.statusText);
   }
-
   return response.json();
 }
-
 async function updatePost(id, postData) {
   const response = await fetch(`${API_URL}/${id}`, {
     method: "PUT",
@@ -45,36 +39,17 @@ async function updatePost(id, postData) {
   }
   return response.json();
 }
-
 async function deletePost(id) {
-  try {
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "DELETE",
-    });
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  });
 
-  //check for HTTP error responses
-  if (!response.ok) {
-    try {
-      const errorData = await response.json();
-      throw new Error(errorDate.message || response.statusText);
-    } catch (jsonError) {
-      // if the error response body isn't JSON or doesn't exist
-      throw new Error(response.statusText);
-    }
-  }
-
-  // For "No Content" status
+  // 204 is No Content status
   if (response.status === 204) {
     return null;
-  } else {
-    return response.json();
   }
-  } catch (error) {
-    // Handle network errors or rethrow the error from above
-  throw new Error(
-    error.message || "An error ocurred while deleting the post. "
-    );
-  }
+
+  throw new Error(response.statusText);
 }
 
 export { createPost, deletePost, fetchAllPosts, fetchPost, updatePost };
